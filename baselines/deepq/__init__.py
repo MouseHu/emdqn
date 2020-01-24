25b492d84d2b0603faa0ba12ce720e0616253a6e
MouseHu <540265415@qq.com>
2020-01-24 13:48:55 +0800

    Add mfvae to see if representation is working. From local.

--- baselines/deepq/__init__.py
+++ baselines/deepq/__init__.py
@@ -3,6 +3,7 @@ from baselines.deepq.build_graph_emdqn import build_act, build_train  # noqa
 from baselines.deepq.build_graph import build_act_dqn, build_train_dqn  # noqa
 from baselines.deepq.build_graph_ibemdqn import build_act_ib, build_train_ib  # noqa
 from baselines.deepq.build_graph_mfec import build_act_mf, build_train_mf  # noqa
+from baselines.deepq.build_graph_mfvae import build_act_mfvae, build_train_mfvae  # noqa
 
 from baselines.deepq.simple import learn, load  # noqa
 from baselines.deepq.replay_buffer import ReplayBuffer, PrioritizedReplayBuffer  # noqa
